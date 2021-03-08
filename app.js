e6018decf99948ad320b62527bd2add8a256e3ad
Bosco_Acer <bosco.chw@gmail.com>
2021-03-08 10:44:29 -0800

    add javascript and css files

---
+++ app.js
@@ -0,0 +1,14 @@
+let num1 = 10
+let num2 = 9
+let string1 = "Hello World"
+let Boo1 = false
+
+if (num1 === num2){
+    console.log("they match")
+} else {
+    console.log("no luck")
+}
+
+for (chr of string1) {
+    console.log(chr)
+}
